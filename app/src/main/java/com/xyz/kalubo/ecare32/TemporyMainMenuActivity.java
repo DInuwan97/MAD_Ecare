fb63199e26271318e0476943a0f902c8a2e29a8c
Dinuwan97 <kalubowila.dc25132@live.com>
2019-08-09 13:03:40 +0530

    Create Tempory Menu Activity

---
+++ app/src/main/java/com/xyz/kalubo/ecare32/TemporyMainMenuActivity.java
@@ -0,0 +1,26 @@
+package com.xyz.kalubo.ecare32;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TemporyMainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempory_main_menu);
+    }
+
+
+    public void showDoctorPortal(View view){
+        Intent intent = new Intent(this,NavigationDrawerActivity.class);
+        startActivity(intent);
+    }
+
+    public void showPatinetPortal(View view){
+        Intent intent = new Intent(this,PatientPortal.class);
+        startActivity(intent);
+    }
+}
